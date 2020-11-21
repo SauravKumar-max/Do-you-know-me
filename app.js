8eb2626a2bbbe9e219e1c485e9689482c28d5358
saurav-max <sauravmax237@gmail.com>
2020-11-21 22:40:05 +0530

    chore: added question

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 var readlineSync = require('readline-sync');
 var score = 0;
 var userName = readlineSync.question("Hello, What is your name? ");
+
 console.log("Hello " + userName + ", lets see how much you know Saurav");
 console.log("-------------------START-------------------");
+
+var qnaData = [
+    {
+      question: "Where do i live?\n",
+      answer: "Hazaribagh"
+    },
+    {
+      question: "What is my favourite fast food?\n",
+      answer: "Samosa"
+    },
+    {
+      question: "Who is my favourite Superhero?\n",
+      answer: "Flash"
+    },
+    {
+      question: "Which is my favourite Anime?\n",
+      answer: "Hunter X Hunter"
+    }
+  ];
