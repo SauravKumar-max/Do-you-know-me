982354d468ea8f93db282fea36026754a4db9a0b
saurav-max <sauravmax237@gmail.com>
2020-11-21 22:16:31 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,5 @@
+var readlineSync = require('readline-sync');
+var score = 0;
+var userName = readlineSync.question("Hello, What is your name? ");
+console.log("Hello " + userName + ", lets see how much you know Saurav");
+console.log("-------------------START-------------------");
